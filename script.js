be071d40929b56cca2a91b5af132b123daf4f111
Kush Agarwal <83411262+Kushhhh-sh@users.noreply.github.com>
2021-08-19 07:31:05 +0000

    Added Chat Screen and message box

---
+++ script.js
@@ -0,0 +1,12 @@
+// var chat = document.getElementsByClassName('chat');
+let connectphone = document.getElementById('div-connectphone');
+let chat1 = document.getElementById('chat-1');
+
+// var chat = document.getElementsByClassName('chat');
+// chat.addEventListener("click", displaynone);
+
+function displaynone(){
+	connectphone.style.display = 'none';
+	chat1.style.display = 'flex';
+}
+
